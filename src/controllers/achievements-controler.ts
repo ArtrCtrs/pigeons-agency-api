166a34ce8304aaaf8aca6bd195eccc9ba593bbee
ArtrCtrs <arthur.catrisse@hotmail.com>
2019-08-26 14:21:08 +0200

    add achievements and merchant

--- src/controllers/achievements-controler.ts
+++ src/controllers/achievements-controler.ts
@@ -1,6 +1,7 @@
 import { AchievementsService } from './../services/achievements-service';
 import { Response, Request } from 'express';
 import { AbstractController } from './abstract-controler';
+import achievementslist from '../lists/achievementsList';
 import { ConnectError } from '../classes/connect-error';
 import globalhelper from '../helpers/globals-helper';
 export class AchievementsControler extends AbstractController {
@@ -13,8 +14,44 @@ export class AchievementsControler extends AbstractController {
 
         res.status(200).send({
             message: 'ok',
-            data: {user:user,userAchievements:userAchievements}
+            data: { user: user, userAchievements: userAchievements }
         });
     }
 
+    static async claimAchievement(req: Request, res: Response) {
+        if (globalhelper.getExpSem()) {
+            console.log("delay");
+            setTimeout(() => AchievementsControler.claimAchievement(req, res), 50);
+        } else {
+            const user = await AchievementsControler.getUserFromRequest(req);
+            await AchievementsControler.updateUserInfo(user);
+
+
+            if (req.body.achievementid == null) {
+                globalhelper.setExpFalse();
+                throw new ConnectError('UNKNOWN_ACHIEVEMENT');
+            }
+            const achievement = achievementslist.filter(obj => {
+                return obj.id === req.body.achievementid
+            })
+            if (achievement == null) {
+                globalhelper.setExpFalse();
+                throw new ConnectError('UNKNOWN_ACHIEVEMENT');
+            }
+            const u: any = user;
+            if (achievement[0].value > (u[achievement[0].attribute])) {
+                globalhelper.setExpFalse();
+                throw new ConnectError('ACHIEVEMENT_REQUIREMENTS');
+            }
+
+            let userAchievements = await AchievementsService.claimAchievement(user, achievement[0]);
+
+            res.status(200).send({
+                message: 'ok',
+                data: { user: user, userAchievements: userAchievements }
+            });
+        }
+
+    }
 }
+
---
+++ src/controllers/merchant-controler.ts
@@ -0,0 +1,57 @@
+import { Response, Request } from 'express';
+import { ConnectError } from "../classes/connect-error";
+import { AbstractController } from './abstract-controler';
+import { MerchantService } from '../services/merchant-service';
+import globalhelper from '../helpers/globals-helper';
+import tradeList from '../lists/tradeList';
+
+export class MerchantControler extends AbstractController {
+    static async honorpointsToFeathers(req: Request, res: Response) {
+        if (globalhelper.getExpSem()) {
+            console.log("delay");
+            setTimeout(() => MerchantControler.honorpointsToFeathers(req, res), 50);
+        } else {
+            let user = await MerchantControler.getUserFromRequest(req);
+
+            if (tradeList[user.lvl].fromHonor > user.honorpoints) {
+                globalhelper.setExpFalse();
+                throw new ConnectError('REQUIREMENTS_ERROR');
+            }
+
+            user.feathers += tradeList[user.lvl].toFeathers;
+            user.honorpoints -= tradeList[user.lvl].fromHonor;
+
+            await MerchantService.honorpointsToFeathers(user);
+
+            globalhelper.setExpFalse();
+            res.status(200).send({
+                message: 'ok',
+                data: user
+            });
+        }
+    }
+    static async feathersToDroppings(req: Request, res: Response) {
+        if (globalhelper.getExpSem()) {
+            console.log("delay");
+            setTimeout(() => MerchantControler.feathersToDroppings(req, res), 50);
+        } else {
+            let user = await MerchantControler.getUserFromRequest(req);
+
+            if (tradeList[user.lvl].fromFeathers > user.feathers) {
+                globalhelper.setExpFalse();
+                throw new ConnectError('REQUIREMENTS_ERROR');
+            }
+
+            user.feathers -= tradeList[user.lvl].fromFeathers;
+            user.droppings += tradeList[user.lvl].toDroppings;
+
+            await MerchantService.feathersToDroppings(user);
+
+            globalhelper.setExpFalse();
+            res.status(200).send({
+                message: 'ok',
+                data: user
+            });
+        }
+    }
+}
