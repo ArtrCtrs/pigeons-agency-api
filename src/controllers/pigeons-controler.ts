265ae409319b6a410b6a1ef2999bddf0800793e2
ArtrCtrs <arthur.catrisse@hotmail.com>
2019-08-28 15:42:39 +0200

    aviary in 1 request

--- src/controllers/pigeons-controler.ts
+++ src/controllers/pigeons-controler.ts
@@ -6,30 +6,163 @@ import globalhelper from '../helpers/globals-helper';
 export class PigeonsControler extends AbstractController {
 
     static async getPigeons(req: Request, res: Response) {
+        if (globalhelper.getExpSem()) {
+            console.log("delayed")
+            setTimeout(() => PigeonsControler.getPigeons(req, res), 50);
+        } else {
+            globalhelper.setExpTrue();
 
-        const user = await PigeonsControler.getUserFromRequest(req);
-        //await PigeonsControler.updateUserInfo(user);
-        
-        if(!req.query.orderby){
-            globalhelper.setExpFalse();
-            throw new ConnectError('INVALID_PARAMETERS');
-        }
-        if(isNaN(req.query.orderby)){
-            globalhelper.setExpFalse();
-            throw new ConnectError('INVALID_PARAMETERS');
-        }
-        let data = await PigeonsService.getPigeons(user.id,req.query.orderby);
-        res.status(200).send({
-            message: 'ok',
-            data: data
-        });
+            let user = await PigeonsControler.getUserFromRequest(req);
+            //await PigeonsControler.updateUserInfo(user);
 
+            if (!req.query.orderby) {
+                globalhelper.setExpFalse();
+                throw new ConnectError('INVALID_PARAMETERS');
+            }
+            if (isNaN(req.query.orderby)) {
+                globalhelper.setExpFalse();
+                throw new ConnectError('INVALID_PARAMETERS');
+            }
+            let pigeons = await PigeonsService.getPigeons(user.id, req.query.orderby);
+
+            user = await PigeonsControler.getUserFromRequest(req);
+            await PigeonsControler.updateUserInfo(user);
+            user = await PigeonsControler.getUserFromRequest(req);
+
+            globalhelper.setExpFalse();
+
+            res.status(200).send({
+                message: 'ok',
+                data: {
+                    user: user,
+                    pigeons: pigeons
+                }
+            });
+        }
+    }
+    static async sellPigeon(req: Request, res: Response) {
+        if (globalhelper.getExpSem()) {
+            console.log("delayed")
+            setTimeout(() => PigeonsControler.sellPigeon(req, res), 50);
+        } else {
+            globalhelper.setExpTrue();
+
+            let user = await PigeonsControler.getUserFromRequest(req);
+            //await PigeonsControler.updateUserInfo(user);
+
+            if (!req.body.pigeonid) {
+                globalhelper.setExpFalse();
+                throw new ConnectError('INVALID_PARAMETERS');
+            }
+            if (!req.query.orderby) {
+                globalhelper.setExpFalse();
+                throw new ConnectError('INVALID_PARAMETERS');
+            }
+            if (isNaN(req.query.orderby)) {
+                globalhelper.setExpFalse();
+                throw new ConnectError('INVALID_PARAMETERS');
+            }
+            await PigeonsService.sellPigeon(user, req.body.pigeonid);
+
+            let pigeons = await PigeonsService.getPigeons(user.id, req.query.orderby);
+
+            user = await PigeonsControler.getUserFromRequest(req);
+            await PigeonsControler.updateUserInfo(user);
+            user = await PigeonsControler.getUserFromRequest(req);
+            globalhelper.setExpFalse();
+
+
+            res.status(200).send({
+                message: 'ok',
+                data: {
+                    user: user,
+                    pigeons: pigeons
+                }
+            });
+        }
+    }
+    static async setAttacker(req: Request, res: Response) {
+        if (globalhelper.getExpSem()) {
+            console.log("delayed")
+            setTimeout(() => PigeonsControler.setAttacker(req, res), 50);
+        } else {
+            globalhelper.setExpTrue();
+
+            let user = await PigeonsControler.getUserFromRequest(req);
+            //await PigeonsControler.updateUserInfo(user);
+            if (!req.body.pigeonid) {
+                globalhelper.setExpFalse();
+                throw new ConnectError('INVALID_PARAMETERS');
+            }
+            if (!req.query.orderby) {
+                globalhelper.setExpFalse();
+                throw new ConnectError('INVALID_PARAMETERS');
+            }
+            if (isNaN(req.query.orderby)) {
+                globalhelper.setExpFalse();
+                throw new ConnectError('INVALID_PARAMETERS');
+            }
+            await PigeonsService.setAttacker(user, req.body.pigeonid);
+
+            let pigeons = await PigeonsService.getPigeons(user.id, req.query.orderby);
+
+            user = await PigeonsControler.getUserFromRequest(req);
+            await PigeonsControler.updateUserInfo(user);
+            user = await PigeonsControler.getUserFromRequest(req);
+            globalhelper.setExpFalse();
+
+            res.status(200).send({
+                message: 'ok',
+                data: {
+                    user: user,
+                    pigeons: pigeons
+                }
+            });
+        }
+    }
+    static async setDefender(req: Request, res: Response) {
+        if (globalhelper.getExpSem()) {
+            console.log("delayed")
+            setTimeout(() => PigeonsControler.setDefender(req, res), 50);
+        } else {
+            globalhelper.setExpTrue();
+
+            let user = await PigeonsControler.getUserFromRequest(req);
+            //await PigeonsControler.updateUserInfo(user);
+            if (!req.body.pigeonid) {
+                globalhelper.setExpFalse();
+                throw new ConnectError('INVALID_PARAMETERS');
+            }
+            if (!req.query.orderby) {
+                globalhelper.setExpFalse();
+                throw new ConnectError('INVALID_PARAMETERS');
+            }
+            if (isNaN(req.query.orderby)) {
+                globalhelper.setExpFalse();
+                throw new ConnectError('INVALID_PARAMETERS');
+            }
+            await PigeonsService.setDefender(user, req.body.pigeonid);
+            let pigeons = await PigeonsService.getPigeons(user.id, req.query.orderby);
+
+            user = await PigeonsControler.getUserFromRequest(req);
+            await PigeonsControler.updateUserInfo(user);
+            user = await PigeonsControler.getUserFromRequest(req);
+            globalhelper.setExpFalse();
+
+            res.status(200).send({
+                message: 'ok',
+                data: {
+                    user: user,
+                    pigeons: pigeons
+                }
+            });
+        }
     }
     // static async addPigeon(req: Request, res: Response) {
 
     //     const user = await PigeonsControler.getUserFromRequest(req);
     //     await PigeonsControler.updateUserInfo(user);
-       
+
     //     await PigeonsService.addPigeon(user.id,req.body.expedition);
     //     res.status(200).send({
     //         message: 'ok',
@@ -37,17 +170,7 @@ export class PigeonsControler extends AbstractController {
     //     });
     // }
 
-    static async sellPigeon(req:Request,res:Response){
-        const user = await PigeonsControler.getUserFromRequest(req);
-        //await PigeonsControler.updateUserInfo(user);
-       
-        await PigeonsService.sellPigeon(user,req.body.pigeonid);
-        res.status(200).send({
-            message: 'ok',
-            data: null
-        });
 
-    }
 
     // static async feedPigeon(req:Request,res:Response){
     //     const user = await PigeonsControler.getUserFromRequest(req);
@@ -55,7 +178,7 @@ export class PigeonsControler extends AbstractController {
     //     if(!req.body.pigeonid){
     //         throw new ConnectError('INVALID_PARAMETERS');
     //     }
-       
+
     //     await PigeonsService.feedPigeon(user,req.body.pigeonid);
     //     res.status(200).send({
     //         message: 'ok',
@@ -63,33 +186,6 @@ export class PigeonsControler extends AbstractController {
     //     });
 
     // }
-    static async setAttacker(req:Request,res:Response){
-        const user = await PigeonsControler.getUserFromRequest(req);
-        //await PigeonsControler.updateUserInfo(user);
-        if(!req.body.pigeonid){
-            globalhelper.setExpFalse();
-            throw new ConnectError('INVALID_PARAMETERS');
-        }
-       
-        await PigeonsService.setAttacker(user,req.body.pigeonid);
-        res.status(200).send({
-            message: 'ok',
-            data: null
-        });
-    }
-    static async setDefender(req:Request,res:Response){
-        const user = await PigeonsControler.getUserFromRequest(req);
-        //await PigeonsControler.updateUserInfo(user);
-        if(!req.body.pigeonid){
-            globalhelper.setExpFalse();
-            throw new ConnectError('INVALID_PARAMETERS');
-        }
-       
-        await PigeonsService.setDefender(user,req.body.pigeonid);
-        res.status(200).send({
-            message: 'ok',
-            data: null
-        });
-    }
+
 
 }
